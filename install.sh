d81c550f31c154e4838aa526acea4c0ad89927dd
Ilario Febi <ifebi@schema31.it>
2016-10-04 12:41:15 +0200

    Mac Os X compatibility

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 cp -a pull.py /usr/local/bin/pull
-cp -a shot.py /usr/local/bin/shot
+if $(uname) == 'Darwin'; then
+    cp -a shot3.py /usr/local/bin/shot
+else
+    cp -a shot.py /usr/local/bin/shot
+fi
+
 chmod 755 /usr/local/bin/pull
 chmod 755 /usr/local/bin/shot
