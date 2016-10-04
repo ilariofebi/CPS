c8f4c1b9c004b815bc5391eddcbe6c40a72261e6
Ilario Febi <ifebi@schema31.it>
2016-10-04 10:06:31 +0200

    Install and uninstall script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp -a pull.py /usr/local/bin/pull
+cp -a shot.py /usr/local/bin/shot
+chmod 755 /usr/local/bin/pull
+chmod 755 /usr/local/bin/shot
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm /usr/local/bin/pull
+rm /usr/local/bin/shot
